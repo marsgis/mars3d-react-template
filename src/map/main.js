7b659af6572f78997fe82e3c8b50fd94f2adab4f
muyao <muyao1987@qq.com>
2020-09-04 21:17:06 +0800

    升级v2.1.2

--- src/map/main.js
+++ src/map/main.js
@@ -27,7 +27,24 @@ export function createMap(id, config) {
 
 //当前页面业务相关
 function initWork(viewer) {
+    // Cesium 1.61以后会默认关闭反走样，对于桌面端而言还是开启得好，
+    viewer.scene.postProcessStages.fxaa.enabled = true;
 
+    // 鼠标滚轮放大的步长参数
+    viewer.scene.screenSpaceCameraController._zoomFactor = 2.0;
+
+    // IE浏览器优化
+    if (window.navigator.userAgent.toLowerCase().indexOf('msie') >= 0) {
+      viewer.targetFrameRate = 20; // 限制帧率
+      viewer.requestRenderMode = true; // 取消实时渲染
+    }
+
+    // 禁用默认的实体双击动作。
+    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+    viewer.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+
+    // 二三维切换不用动画
+    if (viewer.sceneModePicker) { viewer.sceneModePicker.viewModel.duration = 0.0; }
 
 
 }
