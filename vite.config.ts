78ab75a9ed76c5dca4d0fa3b7177d02cc377bab4
wanghao <muyao1987@qq.com>
2022-06-21 14:37:45 +0800

    改为插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mars3dCesium from "./build/cesium-plugin";
+import mars3dCesium from "vite-plugin-mars3d"
 
 // https://vitejs.dev/config/
 export default defineConfig({
