828f4b199d4ebac79ab17b347678fbb03a517340
wanghao <wh@marsgis.cn>
2023-03-17 09:53:14 +0800

    同步代码 v3.5.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import mars3dCesium from "vite-plugin-mars3d"
+import { vitePluginMars3d } from "vite-plugin-mars3d";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    mars3dCesium(),
+    vitePluginMars3d(),
   ],
 });
